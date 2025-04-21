8361cb56421726f7da7b2d4fcf75a6047b8179d6
ingenarel <ingenarelitems@gmail.com>
2025-04-21 18:22:12 +0600

    feat: improved control flow

--- lua/focus-mode/init.lua
+++ lua/focus-mode/init.lua
@@ -1,7 +1,20 @@
 local m = { focusModeOn = false }
 
 function m.toggle()
-	if m.focusModeOn == false then
+	if m.focusModeOn then
+		m.focusModeOn = false
+		vim.opt_local.foldcolumn = "auto"
+		vim.opt_local.colorcolumn = "+0,-20,-40"
+		vim.opt_local.number = true
+		vim.opt_local.relativenumber = true
+		vim.opt_local.signcolumn = "auto"
+		vim.opt_local.cursorcolumn = true
+		vim.opt_local.cursorline = true
+		vim.opt_local.laststatus = 3
+		require("ibl").update({ enabled = true })
+		-- vim.diagnostic.config{virtual_text=true}
+		vim.fn.system({ "tmux", "set", "-g", "status", "on" })
+	else
 		m.focusModeOn = true
 		vim.opt_local.foldcolumn = "0"
 		vim.opt_local.colorcolumn = ""
@@ -15,19 +28,6 @@ function m.toggle()
 		-- TODO: fix this; this is probably fucking crashing clangd
 		-- vim.diagnostic.config{virtual_text=false}
 		vim.fn.system({ "tmux", "set", "-g", "status", "off" })
-	elseif m.focusModeOn == true then
-		m.focusModeOn = false
-		vim.opt_local.foldcolumn = "auto"
-		vim.opt_local.colorcolumn = "+0,-20,-40"
-		vim.opt_local.number = true
-		vim.opt_local.relativenumber = true
-		vim.opt_local.signcolumn = "auto"
-		vim.opt_local.cursorcolumn = true
-		vim.opt_local.cursorline = true
-		vim.opt_local.laststatus = 3
-		require("ibl").update({ enabled = true })
-		-- vim.diagnostic.config{virtual_text=true}
-		vim.fn.system({ "tmux", "set", "-g", "status", "on" })
 	end
 end
 
